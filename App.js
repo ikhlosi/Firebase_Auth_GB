7bce1e27f2c0b01bb117fb704a74af0c2fb084a5
ikhlosi <105212465+ikhlosi@users.noreply.github.com>
2023-05-17 12:03:18 +0200

    Login anonymously

--- App.js
+++ App.js
@@ -3,8 +3,15 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import RootNavigator from "./src/navigation/RootNavigator";
 import AuthUserProvider from "./src/contexts/AuthUserProvider";
+import { useEffect } from "react";
+import { signInAnonymously } from "firebase/auth";
+import { auth } from "./src/firebase";
 
 export default function App() {
+  // Login anonymously
+  useEffect(() => {
+    signInAnonymously(auth).catch((err) => console.log(err));
+  }, []);
   return (
     <AuthUserProvider>
       <RootNavigator />
